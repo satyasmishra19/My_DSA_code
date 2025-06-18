dbbee7ec75a9cdfe8e1fc435bbf9147d67737638
Satyashree Mishra <143271875+satyasmishra19@users.noreply.github.com>
2025-06-18 19:05:07 +0530

    printing the no. of ways to solve and print it in upper side

--- BT_Nqueens.java
+++ BT_Nqueens.java
@@ -11,24 +11,50 @@ public class BT_Nqueens {
             }
         }
 
-        nQueens(board, 0);
-    }
+        for (int i = 0; i < 2; i++) {
+            nQueens(board, 0, i);
+            if (i == 0) {
 
-    public static void nQueens(char board[][], int row) {
-        //base
-        if (row == board.length) {
-            printBoard(board);
-            return;
-        }
-        //column loop
-        for (int i = 0; i < board.length; i++) {
-            if (isSafe(board,row,i)) {
-            board[row][i] = 'Q';
-            nQueens(board, row + 1);//function call
-            board[row][i] = '.';//backtracking step    
+                System.out.println("Total ways to solve n queens = " + count);
             }
         }
     }
+
+    public static void nQueens(char board[][], int row, int j) {
+        //base
+        if (j == 0) {
+            if (row == board.length) {
+                //printBoard(board);
+                count++;
+                return;
+            }
+            //column loop
+            for (int i = 0; i < board.length; i++) {
+                if (isSafe(board, row, i)) {
+                    board[row][i] = 'Q';
+                    nQueens(board, row + 1, j);//function call
+                    board[row][i] = '.';//backtracking step    
+                }
+            }
+        } else {
+            if (row == board.length) {
+                printBoard(board);
+                count++;
+                return;
+            }
+            //column loop
+            for (int i = 0; i < board.length; i++) {
+                if (isSafe(board, row, i)) {
+                    board[row][i] = 'Q';
+                    nQueens(board, row + 1, j);//function call
+                    board[row][i] = '.';//backtracking step    
+                }
+            }
+
+        }
+    }
+
+    static int count = 0;
 
     public static void printBoard(char board[][]) {
         System.out.println("---------CHESS BOARD--------");
@@ -39,21 +65,22 @@ public class BT_Nqueens {
             System.out.println();
         }
     }
-    public static boolean isSafe(char board[][],int row,int col) {
+
+    public static boolean isSafe(char board[][], int row, int col) {
         //vertical up
-        for (int i = row-1; i >= 0; i--) {
+        for (int i = row - 1; i >= 0; i--) {
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
         //diag left up
-        for (int i = row-1, j=col-1; i>=0 && j>=0; i--,j--) {
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
         //diag right up
-        for (int i = row-1, j=col+1; i>=0 && j<board.length; i--,j++) {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
             if (board[i][j] == 'Q') {
                 return false;
             }
